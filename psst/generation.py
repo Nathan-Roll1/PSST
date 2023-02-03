a798b4b7c99cb41e20b0f23266ba8e93d71defcf
Nathan Roll <nroll@ucsb.edu>
2023-02-03 10:33:23 -0800

    generation

---
+++ psst/generation.py
@@ -0,0 +1,9 @@
+def init_model_processor(gpu=False):
+  processor = AutoProcessor.from_pretrained("NathanRoll/psst-medium-en")
+  
+  if gpu:
+    model = AutoModelForSpeechSeq2Seq.from_pretrained("NathanRoll/psst-medium-en").to("cuda:0")
+  else:
+    model = AutoModelForSpeechSeq2Seq.from_pretrained("NathanRoll/psst-medium-en")
+
+  return model, processor
