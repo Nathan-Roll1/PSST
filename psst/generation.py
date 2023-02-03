5c06c94a98a0700eeeb544d1a6acbe3d383a15cb
Nathan-Roll1 <96995554+Nathan-Roll1@users.noreply.github.com>
2023-02-03 11:35:30 -0800

    Delete psst directory

--- psst/generation.py
+++
@@ -1,9 +0,0 @@
-def init_model_processor(AutoProcessor=None, AutoModelForSpeechSeq2Seq=None, gpu=False):
-  processor = AutoProcessor.from_pretrained("NathanRoll/psst-medium-en")
-  
-  if gpu:
-    model = AutoModelForSpeechSeq2Seq.from_pretrained("NathanRoll/psst-medium-en").to("cuda:0")
-  else:
-    model = AutoModelForSpeechSeq2Seq.from_pretrained("NathanRoll/psst-medium-en")
-
-  return model, processor
